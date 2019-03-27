983d6125763effc5299f701e5676fcabfcc0418c
Julián <julian.misiak@gmail.com>
2019-03-27 13:07:24 -0300

    spring boot project

---
+++ src/main/java/com/jas/digitalGourmet/DigitalGourmetApplication.java
@@ -0,0 +1,13 @@
+package com.jas.digitalGourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGourmetApplication.class, args);
+	}
+
+}
