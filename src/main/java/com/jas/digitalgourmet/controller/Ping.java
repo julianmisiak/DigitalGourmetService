d26c6b98b9097a549b0b07c65911c390c3ee952f
JULIAN MISIAK <julian.misiak@gmail.com>
2019-04-02 15:23:10 -0300

    Ping rest swagger configuración

---
+++ src/main/java/com/jas/digitalgourmet/controller/Ping.java
@@ -0,0 +1,19 @@
+package com.jas.digitalgourmet.controller;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class Ping {
+	@GetMapping
+	public ResponseEntity<Date> ping() {
+		return new ResponseEntity<>(new Date(), HttpStatus.OK);
+	}
+
+}
